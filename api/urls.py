03c954916d4cce8c72bae36eee491aaac1e5d104
Sebastian <roncancio.juan@correounivalle.edu.co>
2024-03-28 12:00:49 -0500

    hotfix

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'user_audio_info',views.UserAudioInfoViewSet, basename='user_audio_info')
-router.register(r'audio',views.LoadInputDataViewSet, basename='audio')
+router.register(r'user_audio_info',views.UserAudioInfoViewSet,basename='user_audio_info')
+router.register(r'audio',views.UserAudioInfoViewSet,basename='audio')
 
 urlpatterns= [
     path('',include(router.urls))
